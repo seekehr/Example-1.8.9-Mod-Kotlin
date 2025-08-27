ca96d98d7c9eb72b2f337dbfcc6bb4ce04a8d333
seeker <grouchyseeker@gmail.com>
2025-08-27 23:14:57 +0500

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,20 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     idea
     java
-    id("gg.essential.loom") version "0.10.0.+"
+    id("gg.essential.loom") version "0.10+"
     id("dev.architectury.architectury-pack200") version "0.1.3"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     kotlin("jvm") version "1.9.0"
 }
 
-//Constants:
-
+// Project constants
 val baseGroup: String by project
 val mcVersion: String by project
 val version: String by project
 val mixinGroup = "$baseGroup.mixin"
 val modid: String by project
 
-// Toolchains:
+// Toolchains
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(8))
 }
@@ -28,15 +27,15 @@ sourceSets.main {
     kotlin.destinationDirectory.set(java.destinationDirectory)
 }
 
-// Dependencies:
-
+// Dependencies
 repositories {
     mavenCentral()
     mavenLocal()
 
     maven("https://repo.spongepowered.org/maven/")
+    maven("https://repo.essential.gg/repository/maven-public/")
 
-    // If you don't want to log in with your real minecraft account, remove this line
+    // Optional auth bypass
     maven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1")
 
     maven("https://repo.nea.moe/releases")
@@ -46,11 +45,9 @@ repositories {
 val shadowImpl: Configuration by configurations.creating {
     configurations.implementation.get().extendsFrom(this)
 }
-
 val shadowModImpl: Configuration by configurations.creating {
     configurations.modImplementation.get().extendsFrom(this)
 }
-
 val devenvMod: Configuration by configurations.creating {
     isTransitive = false
     isVisible = false
@@ -61,18 +58,18 @@ dependencies {
     mappings("de.oceanlabs.mcp:mcp_stable:22-1.8.9")
     forge("net.minecraftforge:forge:1.8.9-11.15.1.2318-1.8.9")
 
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib-jdk8")) // keep if you want explicit stdlib
     shadowImpl("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") {
         exclude(group = "org.jetbrains.kotlin")
     }
 
-    // If you don't want mixins, remove these lines
+    // Mixins
     shadowImpl("org.spongepowered:mixin:0.7.11-SNAPSHOT") {
         isTransitive = false
     }
     annotationProcessor("org.spongepowered:mixin:0.8.5-SNAPSHOT")
 
-    // If you don't want to log in with your real minecraft account, remove this line
+    // Optional: DevAuth
     runtimeOnly("me.djtheredstoner:DevAuth-forge-legacy:1.1.2")
 
     shadowModImpl(libs.moulconfig)
@@ -82,24 +79,20 @@ dependencies {
     shadowImpl("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
 }
 
-// Minecraft configuration:
+// Loom config
 loom {
     launchConfigs {
         "client" {
-            // If you don't want mixins, remove these lines
             property("mixin.debug", "true")
             property("asmhelper.verbose", "true")
             arg("--tweakClass", "org.spongepowered.asm.launch.MixinTweaker")
-
             arg("--mods", devenvMod.resolve().joinToString(",") { it.relativeTo(file("run")).path })
         }
     }
     forge {
         pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter())
-        // If you don't want mixins, remove this lines
         mixinConfig("mixins.$modid.json")
     }
-    // If you don't want mixins, remove these lines
     @Suppress("UnstableApiUsage")
     mixin {
         defaultRefmapName.set("mixins.$modid.refmap.json")
@@ -109,35 +102,29 @@ loom {
 kotlin {
     sourceSets.all {
         languageSettings {
-            languageVersion = "2.0"
+            languageVersion = "1.9" // safer than 2.0
             enableLanguageFeature("BreakContinueInInlineLambdas")
         }
     }
 }
 
-// Tasks:
-
+// Tasks
 tasks.compileJava {
     dependsOn(tasks.processResources)
 }
-
-tasks.withType(JavaCompile::class) {
+tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
-
-tasks.withType(Jar::class) {
+tasks.withType<Jar> {
     archiveBaseName.set(modid)
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     manifest.attributes.run {
         this["FMLCorePluginContainsFMLMod"] = "true"
         this["ForceLoadAsMod"] = "true"
-
-        // If you don't want mixins, remove these lines
         this["TweakClass"] = "org.spongepowered.asm.launch.MixinTweaker"
         this["MixinConfigs"] = "mixins.$modid.json"
     }
 }
-
 tasks.processResources {
     inputs.property("version", project.version)
     inputs.property("mcversion", mcVersion)
@@ -147,22 +134,19 @@ tasks.processResources {
     filesMatching(listOf("mcmod.info", "mixins.$modid.json")) {
         expand(inputs.properties)
     }
-
     rename("(.+_at.cfg)", "META-INF/$1")
 }
 
-
+// Remap Jar
 val remapJar by tasks.named<net.fabricmc.loom.task.RemapJarTask>("remapJar") {
     archiveClassifier.set("")
     from(tasks.shadowJar)
     input.set(tasks.shadowJar.get().archiveFile)
 }
-
 tasks.jar {
     archiveClassifier.set("without-deps")
     destinationDirectory.set(layout.buildDirectory.dir("badjars"))
 }
-
 tasks.shadowJar {
     destinationDirectory.set(layout.buildDirectory.dir("badjars"))
     archiveClassifier.set("all-dev")
@@ -174,18 +158,16 @@ tasks.shadowJar {
     }
     exclude("META-INF/versions/**")
 
-    // If you want to include other dependencies and shadow them, you can relocate them in here
     relocate("io.github.moulberry.moulconfig", "$baseGroup.deps.moulconfig")
     relocate("moe.nea.libautoupdate", "$baseGroup.deps.libautoupdate")
 }
-
 tasks.jar {
     archiveClassifier.set("nodeps")
     destinationDirectory.set(layout.buildDirectory.dir("badjars"))
 }
-
 tasks.assemble.get().dependsOn(tasks.remapJar)
 
+// Kotlin compile options
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,10 @@
 pluginManagement {
     repositories {
-        mavenCentral()
         gradlePluginPortal()
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
+        maven("https://repo.essential.gg/repository/maven-public/")
         maven("https://maven.architectury.dev/")
-        maven("https://maven.fabricmc.net")
+        maven("https://maven.fabricmc.net/")
         maven("https://maven.minecraftforge.net/")
-        maven("https://repo.spongepowered.org/maven/")
-        maven("https://repo.sk1er.club/repository/maven-releases/")
     }
     resolutionStrategy {
         eachPlugin {
@@ -19,8 +16,5 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.6.0")
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
 }
-
-
-rootProject.name = "examplemod"
